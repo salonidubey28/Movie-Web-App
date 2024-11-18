4e4adbd06b55703513a1102ddbca3cf192813a00
saloni <saloni22@navgurukul.org>
2024-11-19 01:05:26 +0530

    Fix import path for Axios in Topnav.jsx

--- src/Components/partials/Topnav.jsx
+++ src/Components/partials/Topnav.jsx
@@ -1,4 +1,4 @@
-import axios from '../../utils/Axios';
+import axios from "../../utils/axios";
 import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 import noimage from '/noimage.jpg'
